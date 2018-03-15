41e75245eac9c2811b1f44e64fa01cabe5884605
Myum <muixi.marc@gmail.com>
2018-03-15 18:26:14 +0100

    second release

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 const {app, ipcMain} = require('electron')
 const mainWindow = require('./mainWindow')
 const readItem = require('./readItem')
+const updater = require('./updater')
+
+const {autoUpdater} = require('electron-updater')
+autoUpdater.logger = require("electron-log")
+autoUpdater.logger.transports.file.level = "info"
 
 
 // LIsten for new read item
@@ -20,7 +25,16 @@ ipcMain.on('new-item', (e, itemURL) => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', mainWindow.createWindow)
+app.on('ready', () => {
+  console.log("Create window")
+  autoUpdater.logger.info("message from Main")
+  mainWindow.createWindow()
+
+  setTimeout(() => {
+    console.log("now should update")
+    updater.check()
+  }, 3000)
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
---
+++ updater.js
@@ -0,0 +1,34 @@
+// const {dialog} = require('electron')
+// const {autoUpdater} = require('electron-updater')
+
+// autoUpdater.logger = require("electron-log")
+// autoUpdater.logger.transports.file.level = "info"
+
+// autoUpdater.autoDownload = false
+
+// exports.check = () => {
+//     autoUpdater.logger.info("message from updater")
+//     console.log("Check for updates")
+//     autoUpdater.checkForUpdates()
+
+//     autoUpdater.on('update-available', () => {
+
+//         let downloadProgress = 0
+
+//         dialog.showMessageBox({
+//             type: 'info',
+//             title: 'update available',
+//             message: 'New version available',
+//             buttons: ['Update', 'No']
+//         }, (buttonIndex) => {
+//             if (buttonIndex !== 0) return
+//             autoUpdater.downloadUpdate()
+
+//             autoUpdater.on('download-progress', (d) => {
+//                 downloadProgress = d.percent
+
+//                 autoUpdater.logger.info(downloadProgress)
+//             })
+//         })
+//     })
+// }
